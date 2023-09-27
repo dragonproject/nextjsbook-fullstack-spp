50aaf3f7355cf1083e9e8c61f82b99de6cbb214d
Ryutaro Seki <dragonproject2002@gmail.com>
2023-09-27 13:28:12 +0900

    完了

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,30 @@ import Image from "next/image"
 const ReadAllItems = (props) => {
     return (
         <div>
-            <Head><title>Next Market</title></Head>
+            <Head>
+                <title>Next Market</title>
+            </Head>
             <div className="grid-container-in">
-                {props.allItems.map(item =>
-                    <Link className="card" href={`/item/${item._id}`} key={item._id}>
-                        <Image src={item.image} width={750} height={500} alt="item-image" layout="intrinsic" />
+                {props.allItems.map((item) => (
+                    <Link
+                        className="card"
+                        href={`/item/${item._id}`}
+                        key={item._id}
+                    >
+                        <Image
+                            src={item.image}
+                            width={750}
+                            height={500}
+                            alt="item-image"
+                            layout="intrinsic"
+                        />
                         <div className="texts-area">
                             <h2>¥{item.price}</h2>
                             <h3>{item.title}</h3>
                             <p>{item.description.substring(0, 80)}...</p>
                         </div>
                     </Link>
-                )}
+                ))}
             </div>
         </div>
     )
@@ -25,7 +37,9 @@ const ReadAllItems = (props) => {
 export default ReadAllItems
 
 export const getServerSideProps = async () => {
-    const response = await fetch("https://nextjsbook-fullstack-spp.vercel.app//api/item/readall")
+    const response = await fetch(
+        "https://nextjsbook-fullstack-spp.vercel.app//api/item/readall"
+    )
     const allItems = await response.json()
 
     return {
